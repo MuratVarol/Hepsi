7c65265b8797b4886a81a09a498cc2ed06749d7a
varol <murat.varol@kgteknoloji.com>
2019-03-23 17:04:04 +0300

    list fragment and mainactivity modified for binding on viewModel

---
+++ app/src/main/java/com/varol/hepsi/ui/ListFragment.kt
@@ -0,0 +1,68 @@
+package com.varol.hepsi.ui
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.varol.hepsi.R
+import com.varol.hepsi.base.BaseFragment
+import com.varol.hepsi.databinding.FragmentListBinding
+import com.varol.hepsi.util.listener.EndlessRecyclerViewScrollListener
+import com.varol.hepsi.viewmodel.ListVM
+import observe
+
+class ListFragment : BaseFragment<ListVM, FragmentListBinding>(ListVM::class) {
+
+    override val getLayoutId: Int = R.layout.fragment_list
+
+    private val visibleThreshold = 3
+
+    lateinit var endlessRecyclerViewScrollListener: EndlessRecyclerViewScrollListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        subscribeResetScrollState()
+
+        return binding.root
+    }
+
+    private fun subscribeResetScrollState() {
+        viewModel.isNeedToResetScrollState.observe(this) {
+            if (it == true)
+                endlessRecyclerViewScrollListener.resetState()
+        }
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setRecyclerViewListeners()
+    }
+
+    private fun setRecyclerViewListeners() {
+        val linearLayoutManager = LinearLayoutManager(context)
+        binding.recyclerList.layoutManager = linearLayoutManager
+        endlessRecyclerViewScrollListener = object :
+            EndlessRecyclerViewScrollListener(linearLayoutManager, visibleThreshold) {
+            override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView) {
+                //       viewModel.getAllMovies()
+            }
+        }
+
+        binding.recyclerList.addOnScrollListener(endlessRecyclerViewScrollListener)
+
+//        binding.srl.setOnRefreshListener {
+//            viewModel.getPopularMovies(1)
+//        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/varol/hepsi/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.varol.hepsi.ui
+
+import android.os.Bundle
+import com.varol.hepsi.R
+import com.varol.hepsi.base.BaseActivity
+import com.varol.hepsi.databinding.ActivityMainBinding
+import com.varol.hepsi.viewmodel.MainVM
+
+class MainActivity : BaseActivity<MainVM, ActivityMainBinding>(MainVM::class) {
+    override val layoutRes: Int = R.layout.activity_main
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        loadFragment(R.id.main_container, ListFragment(),true)
+
+    }
+}
