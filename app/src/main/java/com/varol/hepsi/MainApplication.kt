8d10b10ee0423fa9bf961f82dee871612053300e
varol <murat.varol@kgteknoloji.com>
2019-03-23 20:11:51 +0300

    injection modules declared

--- app/src/main/java/com/varol/hepsi/MainApplication.kt
+++ app/src/main/java/com/varol/hepsi/MainApplication.kt
@@ -1,12 +1,28 @@
 package com.varol.hepsi
 
 import android.app.Application
+import com.varol.hepsi.di.networkModule
+import com.varol.hepsi.di.repositoryModule
+import com.varol.hepsi.di.useCaseModule
+import com.varol.hepsi.di.viewModelModule
+import org.koin.android.ext.android.startKoin
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        initKoin()
     }
 
-
+    private fun initKoin() {
+        startKoin(
+            this,
+            listOf(
+                networkModule,
+                viewModelModule,
+                useCaseModule,
+                repositoryModule
+            )
+        )
+    }
 }
