ee2b613177b1e6e1eca21c12909a488cf2ebc0c3
varol <murat.varol@kgteknoloji.com>
2019-03-23 18:20:35 +0300

    return type modified HotDeal to ListModel

--- app/src/main/java/com/varol/hepsi/remote/repository/ListRepository.kt
+++ app/src/main/java/com/varol/hepsi/remote/repository/ListRepository.kt
@@ -1,6 +1,6 @@
 package com.varol.hepsi.remote.repository
 
-import com.varol.hepsi.entities.HotDeal
+import com.varol.hepsi.entities.ListModel
 import com.varol.hepsi.remote.Api
 import com.varol.hepsi.remote.DataHolder
 import io.reactivex.Single
@@ -9,7 +9,7 @@ class ListRepository(
     private val api : Api
 ) : BaseRepository() {
 
-    fun getDealList(page: Int): Single<DataHolder<MutableList<HotDeal>>>
+    fun getDealList(page: Int): Single<DataHolder<MutableList<ListModel>>>
     {
         return service.sendRequest(api.getDealList(page))
     }
