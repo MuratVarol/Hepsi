0c78ff1d72538b8a0908042260fd2e79305f2290
varol <murat.varol@kgteknoloji.com>
2019-03-23 18:16:28 +0300

    getlist use case added

---
+++ app/src/main/java/com/varol/hepsi/usecases/GetListUseCase.kt
@@ -0,0 +1,21 @@
+package com.varol.hepsi.usecases
+
+import android.content.Context
+import com.varol.hepsi.entities.ListModel
+import com.varol.hepsi.remote.DataHolder
+import com.varol.hepsi.remote.repository.ListRepository
+import io.reactivex.Single
+
+class GetListUseCase(
+    private val context: Context,
+    private val listRepository: ListRepository
+) {
+
+    fun getMoviesByType(
+        page: Int
+    ): Single<DataHolder<MutableList<ListModel>>> {
+        return listRepository.getDealList(
+            page
+        )
+    }
+}
