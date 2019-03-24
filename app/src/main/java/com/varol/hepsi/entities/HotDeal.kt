093c5bf613117d1423ebec3a3ee4d443f57df7d2
varol <murat.varol@kgteknoloji.com>
2019-03-25 00:15:39 +0300

    custom getter added for expDate

--- app/src/main/java/com/varol/hepsi/entities/HotDeal.kt
+++ app/src/main/java/com/varol/hepsi/entities/HotDeal.kt
@@ -1,16 +1,17 @@
 package com.varol.hepsi.entities
 
 import android.os.Parcelable
+import com.varol.hepsi.extension.ToDate
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
 data class HotDeal(
     val title : String?,
-    val expirationDate : String?
+    var expirationDate: String?
 ) : Parcelable, Model() {
-    val getValidThruDate : Date?
+    val getExpDate: Date?
     get() {
-       return Calendar.getInstance().time
+        return expirationDate?.ToDate()
     }
 }
